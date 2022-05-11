c663d568558bbaf5ae56044d6e37c2593f2663a8
Yuemin <88663559+Yuem9862@users.noreply.github.com>
2022-05-11 00:40:17 -0400

    Time: 103 ms (59.25%), Space: 51.7 MB (73.90%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -4,24 +4,6 @@
  */
 var maxProfit = function(prices) {
     
-    //solution 1: O(n^2)
-//     if (prices.length<2){
-//       return false;
-//     };
-    
-//     let maxProfit = 0;
-//     for (let i = 0; i< prices.length; i++){
-//      for (let j = i + 1; j <prices.length; j++){
-//          if(prices[j]>prices[i]){
-//            let currentProfit = prices[j] - prices[i];
-//          maxProfit = currentProfit > maxProfit ? currentProfit: maxProfit;
-//          }
-//      }
-//     }
-//     return maxProfit;
-    
-    
-    //solution 2
     let lowestPrice = prices[0];
     let currentPrice;
     let highestProfit = 0;
