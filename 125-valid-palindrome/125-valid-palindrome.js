e4dae9989bd61959c9236b63bbed0abce36c8e5a
Yuemin <88663559+Yuem9862@users.noreply.github.com>
2022-05-11 21:25:46 -0400

    Time: 90 ms (62.33%), Space: 48.7 MB (22.83%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    //convert the string into an array
+    let sArray = s.toLowerCase();
+    sArray = sArray.replace(/[^a-z0-9]/g,'');
+    sArray = sArray.split('');
+    let arrayCopy = [...sArray]
+    
+    //create a stack
+    let stack = [];
+    while(sArray.length !==0){
+        stack.push(sArray.pop());
+    }
+
+    //check
+    if (arrayCopy.join('') ===stack.join('')){
+        return true;
+    }else{
+        return false;
+    }
+
+};
