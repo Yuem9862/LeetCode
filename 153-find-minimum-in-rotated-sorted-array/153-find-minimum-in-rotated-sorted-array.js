a94bd855977a527c86894a5a506b82aca3c118b8
Yuemin <88663559+Yuem9862@users.noreply.github.com>
2022-05-28 17:15:12 -0400

    Time: 77 ms (51.31%), Space: 42.3 MB (25.53%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.js
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.js
@@ -3,6 +3,23 @@
  * @return {number}
  */
 var findMin = function(nums) {
-    nums.sort((a,b)=>a-b);
-    return nums[0];
+    //solution 1: Array.prototype.sort(compareFunc)
+    // nums.sort((a,b)=>a-b);
+    // return nums[0];
+    
+    //solution 2: binary search, two indicators from head/tail to center
+    let left = 0;
+    let right = nums.length - 1;
+    
+    while (left < right){
+        let middle = Math.floor((left + right)/2)
+        if (nums[middle] > nums[right]){
+            left = middle + 1;
+        }else{
+            right = middle;
+        }
+    }
+    
+    return nums[left];
+    
 };
