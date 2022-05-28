695e447f7d49cb05ae16f357e61a9d527b09372e
Yuemin <88663559+Yuem9862@users.noreply.github.com>
2022-05-27 20:13:49 -0400

    Time: 137 ms (54.52%), Space: 53.3 MB (85.83%) - LeetHub

---
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+        
+   
+    
+    let prefix = [];
+    for (let i = 0; i< nums.length; i++){   
+        if (i === 0 ){
+            prefix[i] = 1;
+        }else{
+            prefix[i] = prefix[i-1] * nums[i-1];
+        }
+    }
+    
+    let suffix = [];
+    for (let i = nums.length - 1; i >=0; i--){
+        if (i === nums.length - 1){
+            suffix[i] = 1;
+        }else{
+            suffix[i] = suffix[i+1] * nums[i+1];
+        }
+    }
+    
+    let result = [];
+    for (let i = 0; i <nums.length; i++){
+        result[i] = prefix[i] * suffix[i];
+    }
+    
+    return result;
+    
+};
