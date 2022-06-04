92d7eb36cba339c41ab8f76d1b1c24f068bac42b
Yuemin <88663559+Yuem9862@users.noreply.github.com>
2022-06-04 18:50:55 -0400

    Time: 77 ms (94.17%), Space: 42.4 MB (95.11%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -5,14 +5,42 @@
  */
 let isAnagram = function(s, t) {
 
-    let stringS = s.split('').sort().join('');
-    let stringT = t.split('').sort().join('');
+    // SOLUTION 1: ARRAY
+//     let stringS = s.split('').sort().join('');
+//     let stringT = t.split('').sort().join('');
 
-    if (stringS === stringT){
-        return true;
-    }else{
-        return false;
+//     if (stringS === stringT){
+//         return true;
+//     }else{
+//         return false;
+//     }
+    
+    
+    // SOLUTION 2: HASH TABLE
+    const hash = {};
+    
+    for (let i = 0; i < s.length; i++){
+        let char = s[i];
+        if (!(char in hash)){
+            hash[char] = 0;    
+        }
+        hash[char]++;
+    }
+
+    for (let j = 0; j < t.length; j++){
+        let char2 = t[j];
+        if (char2 in hash){
+            hash[char2]--;
+        }else{
+            return false;
+        }
+    }
+
+    for (const property in hash){
+        if (hash[property] !== 0){
+            return false;
+        }
     }
     
-    
+    return true;
 };
