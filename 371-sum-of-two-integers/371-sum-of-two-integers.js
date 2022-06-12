01cfca4fb7b676774b6f9dd89ca4f5cfc203283b
Yuemin <88663559+Yuem9862@users.noreply.github.com>
2022-06-12 14:22:51 -0400

    Time: 78 ms (45.17%), Space: 41.9 MB (59.97%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.js
+++ 371-sum-of-two-integers/371-sum-of-two-integers.js
@@ -4,10 +4,17 @@
  * @return {number}
  */
 var getSum = function(a, b) {
-    const array = [a,b];
-    const result = array.reduce((pre,cur)=>{
-       return pre = pre + cur;
-    },0);
+    //SOLUTION 1: array.reduce
+//     const array = [a,b];
+//     const result = array.reduce((pre,cur)=>{
+//        return pre = pre + cur;
+//     },0);
     
-    return result;
+//     return result;
+    
+    //SOLUTION 2: Math.pow (**)
+    const a2 = 2**a,
+          b2 = 2**b,
+          c2 = a2 * b2;
+    return Math.log2(c2);
 };
