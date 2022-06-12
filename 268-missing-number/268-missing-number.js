16ad58094e373f1931377fd8af185343df4f8578
Yuemin <88663559+Yuem9862@users.noreply.github.com>
2022-06-12 14:16:44 -0400

    Time: 92 ms (62.20%), Space: 44.8 MB (23.65%) - LeetHub

--- 268-missing-number/268-missing-number.js
+++ 268-missing-number/268-missing-number.js
@@ -3,11 +3,19 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-    let set = new Set (nums)
-    for (let i = 0; i < nums.length; i++){
-        if (set.has(i) === false){
-            return i;
-        }
-    }
-    return nums.length;
+    //SOLUTION 1: Hash table
+    // let set = new Set (nums)
+    // for (let i = 0; i <= nums.length; i++){
+    //     if (set.has(i) === false){
+    //         return i;
+    //     }
+    // }
+    // return false;
+    
+    //SOLUTION 2: Array.reduce
+    const length = nums.length;
+    const sumOfN = (length + 1)*length/2;
+    const sumOfArry = nums.reduce((acc,cur) => acc += cur, 0);
+    return sumOfN - sumOfArry;
+    
 };
