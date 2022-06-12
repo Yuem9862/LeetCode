03d6e826fdbd6a9ee62712c91edc71f617a8ddbc
Yuemin <88663559+Yuem9862@users.noreply.github.com>
2022-06-12 14:57:04 -0400

    Time: 68 ms (70.09%), Space: 41.8 MB (59.97%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.js
+++ 371-sum-of-two-integers/371-sum-of-two-integers.js
@@ -5,16 +5,27 @@
  */
 var getSum = function(a, b) {
     //SOLUTION 1: array.reduce
-//     const array = [a,b];
-//     const result = array.reduce((pre,cur)=>{
-//        return pre = pre + cur;
-//     },0);
+    // const array = [a,b];
+    // const result = array.reduce((pre,cur)=>{
+    //      return pre = pre + cur;
+    // },0);
     
-//     return result;
+    // return result;
     
     //SOLUTION 2: Math.pow (**)
-    const a2 = 2**a,
-          b2 = 2**b,
-          c2 = a2 * b2;
-    return Math.log2(c2);
+    // const a2 = 2**a,
+    //       b2 = 2**b,
+    //       c2 = a2 * b2;
+    // return Math.log2(c2);
+    
+    //SOLUTION 3: bit manipulation 
+    // XOR ^ for adding, AND & for carry
+    
+    let temp;
+    while (b !==0){
+        temp = (a & b) << 1;
+        a = a ^ b;
+        b = temp;
+    }
+    return a;
 };
