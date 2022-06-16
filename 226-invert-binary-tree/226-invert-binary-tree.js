b7028025199114a5075b9bc6d979a32c79f6c84f
Yuemin <88663559+Yuem9862@users.noreply.github.com>
2022-06-15 21:35:34 -0400

    Time: 92 ms (32.33%), Space: 42.5 MB (42.35%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.js
+++ 226-invert-binary-tree/226-invert-binary-tree.js
@@ -11,13 +11,41 @@
  * @return {TreeNode}
  */
 var invertTree = function(root) {
-    if (!root) return null;
-    let temp = root.left;
-    root.left = root.right;
-    root.right = temp;
+    //SOLUTION 1: Recursive
+//     if (!root) return null;
+//     let temp = root.left;
+//     root.left = root.right;
+//     root.right = temp;
 
-    invertTree(root.left);
-    invertTree(root.right);
+//     invertTree(root.left);
+//     invertTree(root.right);
+    
+//     return root;
+    
+    //SOLUTION 2: Iterative (BFS)
+       
+    let queue = [root];
+    
+    while (queue.length > 0){
+        
+        const child = []
+        
+        for (const node of queue){
+            if (!node){
+                continue;
+            }
+            
+            const temp = node.left;
+            node.left = node.right;
+            node.right = temp;
+            
+            child.push(node.left);
+            child.push(node.right);
+            
+        }
+        queue = child;
+    }
     
     return root;
+    
 };
