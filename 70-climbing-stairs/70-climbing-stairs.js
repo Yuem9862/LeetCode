de1bd7a05e29e0c74ae62ad5611ac886a71b265b
Yuemin <88663559+Yuem9862@users.noreply.github.com>
2022-07-06 22:13:46 -0400

    Time: 92 ms (35.17%), Space: 42 MB (30.63%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.js
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -3,19 +3,43 @@
  * @return {number}
  */
 
-
-let cache = {};
- 
-
-var climbStairs = function(n) {
-    if (n in cache){
-           return cache[n];
-    }else{
-        if(n<=2){
-            return n;
-        }else{
-            cache[n] = climbStairs(n-1) + climbStairs(n-2);
+    //DP
+    let cache = {};
+    
+    function climbStairs (n){
+        if (n in cache){
             return cache[n];
-        };
-    };
-};
+        }else{
+            if (n<=2){
+                return n;
+            }else{
+                cache[n] = climbStairs(n-1) + climbStairs(n-2);
+                return cache[n];
+            }
+        }
+    }
+
+    
+    //CLOSURE
+//     var climbStairs = function (){
+
+//         let cache = {};
+
+//          function climbClosure (n){
+//             if (n in cache){
+//                 return cache[n];
+//             }else{
+//                 if (n<=2){
+//                     return n;
+//                 }else{
+//                     cache[n] = climbClosure(n-1) + climbClosure(n-2);
+//                     return cache[n];
+//                 }
+//             }
+//         }
+
+//       return climbClosure;
+//     }
+
+//     const f = climbStairs();
+//     console.log(f(5))
