3384c00ad41a92926df1737860f2de255b19e827
Yuemin <88663559+Yuem9862@users.noreply.github.com>
2022-07-23 22:01:19 -0400

    Time: 94 ms (75.66%), Space: 43.1 MB (33.54%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,22 +4,39 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+// Option 1: O(n^2)
+//     let result = [];
     
-    let result = [];
+//     if (nums.length < 2){
+//         return result;
+//     }
     
-    if (nums.length < 2){
-        return result;
+    
+//     for (let i = 0 ; i < nums.length - 1; i++){    
+//         for (let j = i + 1; j < nums.length; j ++){
+//           if (nums[i] + nums[j] === target){
+//               result = [i,j];
+//               return result;
+//           }  
+//         }      
+//     }
+    
+//   return result;
+    
+    
+    
+//use map to store value - index pair
+  const map = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const num1 = nums[i]
+    const num2 = target - num1;
+
+    if (map[num2] >= 0) {
+      return [i, map[num2]];
+    } else {
+      map[num1] = i;
     }
+  }
     
-    
-    for (let i = 0 ; i < nums.length - 1; i++){    
-        for (let j = i + 1; j < nums.length; j ++){
-          if (nums[i] + nums[j] === target){
-              result = [i,j];
-              return result;
-          }  
-        }      
-    }
-    
-    return result;
 };
