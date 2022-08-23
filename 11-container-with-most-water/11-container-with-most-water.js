55db0ca6d42633c5a1004392eea6579ee01711a1
Yuemin <88663559+Yuem9862@users.noreply.github.com>
2022-08-23 19:38:39 -0400

    Time: 99 ms (71.47%), Space: 49.8 MB (49.95%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.js
+++ 11-container-with-most-water/11-container-with-most-water.js
@@ -3,23 +3,22 @@
  * @return {number}
  */
 var maxArea = function(height) {
-   let left = 0,
-       right = height.length - 1,
-       maxWater = null;
+    let left = 0;
+    let right = height.length - 1;
+    let maxWater = 0;
     
     while (left < right){
         
-        const water = (right - left) * Math.min(height[left], height[right]);
+        let water = (right - left) * Math.min(height[left], height[right])
         maxWater = Math.max(maxWater, water);
         
-        if (height[left] <= height[right]){
-            left++;
-        }else{     
-            right--;
+        if (height[left] < height[right]){
+            left ++;
+        }else{
+            right --;
         }
-        
-      
     }
     
-    return maxWater;
+    return maxWater
+    
 };
