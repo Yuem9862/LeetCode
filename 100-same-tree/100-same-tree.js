9ab66b6ed981b13a0548abe75bf305297060ea74
Yuemin <88663559+Yuem9862@users.noreply.github.com>
2022-08-23 19:11:48 -0400

    Time: 70 ms (80.67%), Space: 42.2 MB (62.62%) - LeetHub

--- 100-same-tree/100-same-tree.js
+++ 100-same-tree/100-same-tree.js
@@ -13,43 +13,15 @@
  */
 var isSameTree = function(p, q) {
     
-    //SOLUTION 1: Recursive
-//     if (p === null && q === null){
-//         return true;
-//     }
-    
-//     if ((p == null && q != null) || (q == null && p != null)) {
-// 		return false;
-// 	}
-    
-//     if (p.val !== q.val){
-//         return false;
-//     }
-        
-//     return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
-    
-    //SOLUTION 2: Iterative
-    
-    let stack = [];
-    stack.push([p, q]);
-    
-    while (stack.length !==0){
-        let nodes = stack.pop();
-        let n1 = nodes[0];
-        let n2 = nodes[1];
-        if (!n1 && !n2) continue;
-        if (!n1 || !n2) {
-            return false;
-        }
-        if (n1.val !== n2.val) {
-            return false;
-        }
-        stack.push([n1.left, n2.left]);
-        stack.push([n1.right, n2.right]);        
+    //base value
+    if (!p && !q){
+        return true;
     }
-    return true;
-}
     
-   
+    if (!q && p || q && !p || p.val !== q.val){
+        return false;
+    }
     
-  
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    
+};
