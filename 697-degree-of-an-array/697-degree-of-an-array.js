5ce3f9aa82010a998795fe1158e765e55289eff6
Yuemin <88663559+Yuem9862@users.noreply.github.com>
2022-08-26 23:14:16 -0400

    Time: 154 ms (45.01%), Space: 46.5 MB (51.85%) - LeetHub

---
+++ 697-degree-of-an-array/697-degree-of-an-array.js
@@ -0,0 +1,58 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findShortestSubArray = function(nums) {
+    
+    //find the degree
+    
+    let map = {};
+    
+    for (let i = 0; i < nums.length; i++){
+        if (!map[nums[i]]){
+            map[nums[i]] = 0;
+        }
+        map[nums[i]]++;
+    }
+    
+    let arrOfValues = [];
+    
+    for (const key in map){
+        arrOfValues.push(map[key]);
+    }
+    
+    const maxValue = arrOfValues.sort((a,b)=>b-a)[0];
+    
+    const degrees = Object.keys(map).filter(key => map[key] === maxValue);
+    
+    
+    //find the starting and ending index of the degree
+    
+    let ans = Infinity;
+    
+    for (const degreeStre of degrees){
+    
+        const degree = parseInt(degreeStre)
+ 
+        let startingIndex = 0;
+        let endingIndex = nums.length - 1;
+    
+        for (let i = 0; i < nums.length; i++){
+            if (nums[i] === degree){
+                startingIndex = i;
+                break;
+            }    
+        }
+    
+        for (let i = nums.length - 1; i >= 0; i--){
+            if (nums[i] === degree){
+                endingIndex = i;
+                break;
+            }
+        }
+    
+        ans = Math.min(ans, endingIndex - startingIndex + 1)
+    }
+    
+    return ans;
+};
