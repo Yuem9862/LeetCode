b3391664fc70a199d6ddb5eaea4187a238b9d3f3
Yuemin <88663559+Yuem9862@users.noreply.github.com>
2022-08-29 21:39:48 -0400

    Time: 71 ms (81.19%), Space: 41.5 MB (98.91%) - LeetHub

---
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+       
+    //iterative - using stack
+    
+    if (!root){
+        return [];
+    };
+    
+    let stack = [root];
+    let ans= []
+       
+    while(stack.length){
+        const curr = stack.pop();
+        ans.push(curr.val);
+        
+        if(curr.right){
+            stack.push(curr.right)
+        };
+        if(curr.left){
+            stack.push(curr.left)
+        };
+    }
+    
+    return ans;
+    
+    // recursive
+};
