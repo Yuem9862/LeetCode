f70bc72ccf980816d0db66471bdc8bd27e30dbb5
Yuemin <88663559+Yuem9862@users.noreply.github.com>
2022-08-29 21:45:46 -0400

    Time: 89 ms (47.88%), Space: 42 MB (69.39%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.js
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.js
@@ -14,26 +14,47 @@ var preorderTraversal = function(root) {
        
     //iterative - using stack
     
-    if (!root){
-        return [];
-    };
+//     if (!root){
+//         return [];
+//     };
     
-    let stack = [root];
-    let ans= []
+//     let stack = [root];
+//     let ans= []
        
-    while(stack.length){
-        const curr = stack.pop();
-        ans.push(curr.val);
+//     while(stack.length){
+//         const curr = stack.pop();
+//         ans.push(curr.val);
         
-        if(curr.right){
-            stack.push(curr.right)
-        };
-        if(curr.left){
-            stack.push(curr.left)
-        };
-    }
+//         if(curr.right){
+//             stack.push(curr.right)
+//         };
+//         if(curr.left){
+//             stack.push(curr.left)
+//         };
+//     }
     
-    return ans;
+//     return ans;
     
     // recursive
+    
+    let ans = [];
+    
+    function traverse(root){
+        if (!root){
+            return;
+        }
+
+        ans.push(root.val);
+
+        if (root.left){
+            traverse(root.left);
+        }
+        if (root.right){
+            traverse(root.right);
+        }
+    }
+    
+    traverse(root);
+    
+    return ans;
 };
